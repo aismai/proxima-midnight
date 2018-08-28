874b9460fe9662052924af135b056794b7ae9f28
aismai <abai.ismailov@gmail.com>
2018-08-28 16:09:28 +0600

    add Layout components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
 import "./App.css";
 
+import Navbar from "./components/Layout/Navbar";
+import Landing from "./components/Layout/Landing";
+import Footer from "./components/Layout/Footer";
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Annie's Collaboration</h1>
+        <Navbar />
+        <Landing />
+        <Footer />
       </div>
     );
   }
