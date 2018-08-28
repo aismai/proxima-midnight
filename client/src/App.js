ba5a5d11269c16b5e09c21f005081a0f92a6832c
aismai <abai.ismailov@gmail.com>
2018-08-28 16:38:42 +0600

    add react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,27 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 
 import Navbar from "./components/Layout/Navbar";
 import Landing from "./components/Layout/Landing";
 import Footer from "./components/Layout/Footer";
+import Register from "./components/Auth/Register";
+import Login from "./components/Auth/Login";
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Landing />
-        <Footer />
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <div className="container">
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </div>
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
