bdc331197fb24f5a3075c092c74361a5575c8a32
aismai <abai.ismailov@gmail.com>
2018-08-29 16:07:35 +0600

    register users with redux actions
    add redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
+import { Provider } from "react-redux";
+import store from "./store";
 
 import Navbar from "./components/Layout/Navbar";
 import Landing from "./components/Layout/Landing";
@@ -11,17 +13,19 @@ import Login from "./components/Auth/Login";
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Navbar />
-          <Route exact path="/" component={Landing} />
-          <div className="container">
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <div className="container">
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </div>
+            <Footer />
           </div>
-          <Footer />
-        </div>
-      </Router>
+        </Router>
+      </Provider>
     );
   }
 }
