16649a92eb1e76e7d0be2a238510d6d22f2daa1d
aismai <abai.ismailov@gmail.com>
2018-09-02 17:44:53 +0600

    add Profiles component

--- client/src/App.js
+++ client/src/App.js
@@ -20,6 +20,7 @@ import CreateProfile from "./components/CreateProfile/CreateProfile";
 import EditProfile from "./components/EditProfile/EditProfile";
 import AddExperience from "./components/AddCredentials/AddExprerience";
 import AddEducation from "./components/AddCredentials/AddEducation";
+import Profiles from "./components/Profiles/Profiles";
 
 import "./App.css";
 
@@ -56,6 +57,7 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <Route exact path="/profiles" component={Profiles} />
               <Switch>
                 <PrivateRoute exact path="/dashboard" component={Dashboard} />
               </Switch>
