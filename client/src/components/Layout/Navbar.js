b4fb43ec8e4d9f4cd034f2f26a0058cbad7140c0
aismai <abai.ismailov@gmail.com>
2018-09-02 13:24:19 +0600

    add EditProfile component

--- client/src/components/Layout/Navbar.js
+++ client/src/components/Layout/Navbar.js
@@ -18,6 +18,11 @@ class Navbar extends Component {
     const authLinks = (
       <ul className="navbar-nav ml-auto">
         <li className="nav-item">
+          <Link className="nav-link" to="/dashboard">
+            Dashboard
+          </Link>
+        </li>
+        <li className="nav-item">
           <a href="" onClick={this.onLogoutClick} className="nav-link">
             <img
               src={user.avatar}
@@ -65,7 +70,6 @@ class Navbar extends Component {
             <ul className="navbar-nav mr-auto">
               <li className="nav-item">
                 <Link className="nav-link" to="/profiles">
-                  {" "}
                   Developers
                 </Link>
               </li>
