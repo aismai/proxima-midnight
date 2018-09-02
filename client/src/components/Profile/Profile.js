df31eaa7249a78265e607b3fc547f2543a5866f2
aismai <abai.ismailov@gmail.com>
2018-09-02 19:39:44 +0600

    fill Profile component with data

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -11,19 +11,47 @@ import ProfileGithub from "./ProfileGithub";
 import Spinner from "../Common/Spinner/Spinner";
 
 class Profile extends Component {
-  componentDidMount = () => {
+  componentDidMount() {
     if (this.props.match.params.handle) {
       this.props.getProfileByHandle(this.props.match.params.handle);
     }
-  };
+  }
 
   render() {
+    const { profile, loading } = this.props.profile;
+    let profileContent;
+
+    if (profile === null || loading === true) {
+      profileContent = <Spinner />;
+    } else {
+      profileContent = (
+        <div>
+          <div className="row">
+            <div className="col-md-6">
+              <Link to="/profiles" className="btn btn-light mb-3 float-left">
+                Back to profiles
+              </Link>
+            </div>
+            <div className="col-md-6" />
+          </div>
+          <ProfileHeader profile={profile} />
+          <ProfileAbout profile={profile} />
+          <ProfileCreds
+            education={profile.education}
+            experience={profile.experience}
+          />
+          <ProfileGithub />
+        </div>
+      );
+    }
+
     return (
-      <div>
-        <ProfileHeader />
-        <ProfileAbout />
-        <ProfileCreds />
-        <ProfileGithub />
+      <div className="profile">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">{profileContent}</div>
+          </div>
+        </div>
       </div>
     );
   }
